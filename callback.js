98719c2c0cb730182647e32ff861179997160ffc
Devesh <dev250701@gmail.com>
2023-04-08 18:17:41 +0530

    K

--- callback.js
+++ callback.js
@@ -1,28 +1,29 @@
 // Code1
-// function doSomethingAsync(value, callback) {
-//     setTimeout(function() {
-//         const result1 = Math.random();
-//         callback(result1*value);
-//     }, 5000);
-// };
+function doSomethingAsync(value, callback) {
+    setTimeout(function() {
+        const result1 = Math.random();
+        callback(result1*value);
+    }, 5000);
+};
   
-// doSomethingAsync(3, function(result) {
-//     console.log('The result is:', result);
-// });
+doSomethingAsync(3, function(result) {
+    console.log('The result is:', result);
+});
 
 
 // Code 2
-// function Name(value, callbackk){
-//     setTimeout(function(){
-//         callbackk('Devesh' + value);
-//     },4000);
-// };
+function Name(value, callbackk){
+    setTimeout(function(){
+        callbackk('Devesh' + value);
+    },4000);
+};
 
-// Name('Sharma', function(result1){
-//     console.log(result1)
-// })
+Name('Sharma', function(result1){
+    console.log(result1)
+})
 
 
+// Code3
 const nname = (value,callback) =>{
     setTimeout(()=>{
         callback('Devesh'+value)
@@ -35,15 +36,13 @@ nname('Sharma',(fullname)=>{
 
 
 
-
+// Code 4
 // Code for synchronous Function
-// const add = (a,b,cb) => {
-//     const c  = a + b;
-//     cb(c);
-// }
+const add = (a,b,cb) => {
+    const c  = a + b;
+    cb(c);
+}
 
-// add(1,2,(no) =>{
-//     console.log(no);
-// })
-
-
+add(1,2,(no) =>{
+    console.log(no);
+})
