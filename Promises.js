02314ab3601b9e7ab1f7474e563e75eebcf793aa
Devesh <dev250701@gmail.com>
2023-05-31 16:49:42 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -33,3 +33,72 @@ add(1,'we')
 .catch((error) => {
     console.log(error)
 })
+
+// Code 3
+let p = (a,b) =>{
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            let sum = a + b
+            if (sum > 5) resolve('True')
+            else reject('False')
+        },2000)
+    });
+}
+
+p(3,'dd')
+.then((result) => {
+    console.log(result)
+})
+.catch((error) => {
+    console.error(error)
+})
+
+
+// Code4
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        let sum = a*b
+        if (sum) resolve(sum)
+        else reject(new Error('Not Possible'))
+    });
+}
+
+add(1,)
+.then((result) => {
+    console.log(result)
+})
+.catch((error) => {
+    console.log(error)
+})
+
+
+// Code5
+let f = (c,d) => {
+    return new Promise((resolve,reject) =>{
+        setTimeout(() =>{
+            let add = c+d
+            if (add>5) resolve('True')
+            else reject('False')
+        },3000);
+    })
+}
+
+f(3,'sd')
+.then((result) =>{
+    console.log(result)
+})
+.catch((error) =>{
+    console.log(error)
+})
+
+// CODE 5 for async await
+async function main_functon(c,d) {
+    try {
+        let data = await f(c,d);
+        console.log(data);
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+main_functon(2,7);
